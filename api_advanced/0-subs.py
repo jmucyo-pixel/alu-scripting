640056dfbbb6502ed6bb2e306e093c63e0a01d01
jmucyo-pixel <j.mucyo@alustudent.com>
2026-07-21 16:22:38 +0200

    1st project in api advanced

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+0-subs module
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. If an invalid subreddit is given,
+    returns 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "python:subs.checker:v1.0 (by /u/example)"}
+
+    try:
+        response = requests.get(
+            url,
+            headers=headers,
+            allow_redirects=False
+        )
+    except requests.exceptions.RequestException:
+        return 0
+
+    if response.status_code != 200:
+        return 0
+
+    try:
+        data = response.json()
+        return data.get("data", {}).get("subscribers", 0)
+    except ValueError:
+        return 0
