6d889a725fd09c486bfccb1c4cef7d86d00ecdfb
joelm <j.mucyo@alustudent.com>
2026-07-22 18:02:38 +0200

    2nd project top 10

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,45 +1,16 @@
 #!/usr/bin/python3
-"""
-1-top_ten module
-"""
+"""Module that queries the Reddit API for top 10 hot posts."""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot
-    posts listed for a given subreddit. If the subreddit is invalid,
-    prints None.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "python:top_ten.checker:v1.0 (by /u/example)"}
-    params = {"limit": 10}
-
-    try:
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False
-        )
-    except requests.exceptions.RequestException:
-        print(None)
-        return
-
+    """Print the titles of the first 10 hot posts for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "linux:myredditapp:v1.0 (by /u/yourusername)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
-
-    try:
-        data = response.json()
-        posts = data.get("data", {}).get("children", [])
-    except ValueError:
-        print(None)
-        return
-
-    if not posts:
-        print(None)
-        return
-
+    posts = response.json().get("data", {}).get("children", [])
     for post in posts:
         print(post.get("data", {}).get("title"))
